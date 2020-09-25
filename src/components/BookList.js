c334e33a23fa5974631e4944ca083e47a2cf968a
Azeem <azeemahmed101@gmail.com>
2020-09-25 17:50:44 +0200

    update bookList to receive from store

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Book from './Book';
 
 const BookList = ({ books }) => {
+  const bookList = books.map(book => (
+    <Book book={book} key={Math.random()} />
+  ));
   return (
     <table>
       <tr>
@@ -9,20 +14,21 @@ const BookList = ({ books }) => {
         <th>Title</th>
         <th>Category</th>
       </tr>
-      <tr>
-        <td>{books[0].id}</td>
-        <td>{books[0].title}</td>
-        <td>{books[0].category}</td>
-      </tr>
+      {bookList}
     </table>
-  )
-}
+  );
+};
 
-const mapStateToProps = (state) => {
-  return {
-    books: state.books
-  }
-}
+const mapStateToProps = state => ({
+  books: state.books,
+});
 
+BookList.propTypes = {
+  books: PropTypes.objectOf,
+};
+
+BookList.defaultProps = {
+  books: [],
+};
 
 export default connect(mapStateToProps)(BookList);
