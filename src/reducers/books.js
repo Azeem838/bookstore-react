0812d003db12f0394f256ec76e823ae11459f108
Azeem <azeemahmed101@gmail.com>
2020-09-25 17:51:09 +0200

    add book reducer

---
+++ src/reducers/books.js
@@ -0,0 +1,19 @@
+const bookReducer = (state, action) => {
+  if (action.type === 'CREATE_BOOK') {
+    const newBooks = [...state.books, action.book];
+    return {
+      ...state,
+      books: newBooks,
+    };
+  }
+  if (action.type === 'REMOVE_BOOK') {
+    const newBooks = state.books.filter(book => book.id !== action.book.id);
+    return {
+      ...state,
+      books: newBooks,
+    };
+  }
+  return state;
+};
+
+export default bookReducer;
