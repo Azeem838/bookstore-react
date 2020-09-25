956f9166543ef61e5aa7c62d718f027e817b2689
Azeem <azeemahmed101@gmail.com>
2020-09-25 17:49:49 +0200

    add bookForm

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,11 +1,34 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-export default class BookForm extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
-}
+const BookForm = () => {
+  const categories = [
+    'Action',
+    'Biography',
+    'History',
+    'Horror',
+    'Kids',
+    'Learning',
+    'Sci-Fi',
+  ];
+  const catList = categories.map(cat => (
+    <option key={Math.random()} value={cat}>
+      {cat}
+    </option>
+  ));
+
+  return (
+    <form>
+      <label htmlFor="title">
+        Title:
+        <input type="text" id="title" />
+      </label>
+
+      <select name="categories" id="">
+        {catList}
+      </select>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default BookForm;
