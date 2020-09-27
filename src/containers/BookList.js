1f2e81ca62e8ae3c9a6f138b8b52b8d758887b12
Phillip Musiime <phillipmusiime@gmail.com>
2020-09-27 12:39:09 +0300

    create handleFilter method and implement filter logic

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -2,50 +2,69 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Book from '../components/Book';
-import { removeBook } from '../actions/index';
+import { changeFilter, removeBook } from '../actions/index';
+import CategoryFilter from './CategoryFilter';
 
 class BookList extends Component {
   constructor(props) {
     super(props);
 
     this.removeBook = this.removeBook.bind(this);
+    this.handleFilter = this.handleFilter.bind(this);
   }
 
   removeBook(id) {
-    const { removeBook } = this.props;
-    removeBook(id);
+    this.props.removeBook(id);
+  }
+
+  handleFilter(e) {
+    this.props.changeFilter(e.target.value)
   }
 
   render() {
-    const { books } = this.props;
+    const { books, filter } = this.props;
+    let bookList;
 
-    const bookList = books.map(book => (
-      <Book removeBook={this.removeBook} book={book} key={Math.random()} />
-    ));
+    if (filter === 'All') {
+      bookList = books.map(book => (
+        <Book removeBook={this.removeBook} book={book} key={Math.random()} />
+      ));
+    } else {
+      bookList = books.filter(book => book.category === filter).map(book => (
+        <Book removeBook={this.removeBook} book={book} key={Math.random()} />
+      ));
+    }
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>Book ID</th>
-            <th>Title</th>
-            <th>Category</th>
-            <th>Remove Book</th>
-          </tr>
-        </thead>
-        <tbody>{bookList}</tbody>
-      </table>
+      <div>
+        <CategoryFilter handleFilter={this.handleFilter} />
+        <table>
+          <thead>
+            <tr>
+              <th>Book ID</th>
+              <th>Title</th>
+              <th>Category</th>
+              <th>Remove Book</th>
+            </tr>
+          </thead>
+          <tbody>{bookList.length ? bookList : "No Books in this Category!"}</tbody>
+        </table>
+      </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
   books: state.books,
+  filter: state.filter
 });
 
 const mapDispatchToProps = dispatch => ({
   removeBook: id => {
     dispatch(removeBook(id));
   },
+  changeFilter: filter => {
+    dispatch(changeFilter(filter))
+  }
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(BookList);
