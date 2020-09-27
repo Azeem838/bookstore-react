e4d86cab9591e8343a602fa6671fd4b35dcca479
Azeem <azeemahmed101@gmail.com>
2020-09-27 18:40:46 +0200

    add card layout and style

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,19 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../styles/bookCard.css';
 
 const Book = ({ book, removeBook }) => {
   const { id, title, category } = book;
   return (
-    <tr>
-      <td>{id}</td>
-      <td>{title}</td>
-      <td>{category}</td>
-      <td>
-        <button type="button" onClick={() => removeBook(id)}>
-          Remove Book
-        </button>
-      </td>
-    </tr>
+    <div className="card-container">
+      <div className="book-info">
+        <div className="category">{category}</div>
+        <div className="title">{title}</div>
+        <div className="author">Author</div>
+        <div className="card-links">
+          <button type="button">Comments</button>
+          <button type="button" onClick={() => removeBook(id)}>
+            Remove
+          </button>
+          <button type="button">Edit</button>
+        </div>
+      </div>
+
+      <div className="progress-info">
+        <div className="progress-center">
+          <svg>
+            <circle cx="70" cy="70" r="34" />
+            <circle cx="70" cy="70" r="34" />
+          </svg>
+          <div className="percent-of-circle">
+            <div className="percent-completed">64%</div>
+            <div className="completed">Completed</div>
+          </div>
+        </div>
+        <div className="progress-update">
+          <div className="current-chapter">Current Chapter</div>
+          <div className="chapter">Chapter 17</div>
+          <button type="button" className="progress-btn">
+            Update Progress
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
