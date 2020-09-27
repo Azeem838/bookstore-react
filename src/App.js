5b0052881e38b2c6c5926d0e52a823c2877e07f6
Phillip Musiime <phillipmusiime@gmail.com>
2020-09-27 11:07:58 +0300

    display categoryFilter component in app components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import BookList from './containers/BookList';
 import BookForm from './containers/BookForm';
+import CategoryFilter from './containers/CategoryFilter';
 
 function App() {
   return (
     <div>
+      <CategoryFilter />
       <BookList />
       <BookForm />
     </div>
